629df075261fc69dd1573ebb6722ebee988d50ac
unknown <uwasedamztoni@gmail.com>
2024-02-15 15:21:40 +0200

    chenges in index file
    added boostrap

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>schoolsmis</title>
+</head>
+<body>
+<?php>
+echo"<h3>welcame to this page </h3>" 
+</body>
+</html>
+
